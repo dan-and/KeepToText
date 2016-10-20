92238903dc8f0733cf9a5d686c545c5f763984a3
Tzadik Vanderhoof <Tzadik.Vanderhoof@gmail.com>
2016-10-19 20:58:13 -0700

    print files

--- html-to-text.py
+++ html-to-text.py
@@ -1,12 +1,25 @@
-import sys
+import sys, glob, os
+
+def htmlToText(dir, tag, attrib, attribVal):
+    try:
+        os.chdir(dir)
+    except WindowsError as e:
+        sys.exit(e)
+    
+    outputDir = dir + "/Output"
+    
+    if not os.path.isdir(outputDir):
+        os.mkdir(outputDir)
+    
+    for fname in glob.glob("*.html"):
+        print fname
 
 def main():
     try:
         cmd, dir, tag, attrib, attribVal = sys.argv
-    except ValueError as e:
-        print "Usage:", sys.argv[0], "dir tag attrib attribVal"
-        return
-    print(tag)
+    except ValueError:
+        sys.exit("Usage: {0} dir tag attrib attribVal".format(sys.argv[0]))
+    htmlToText(dir, tag, attrib, attribVal)
 
 if __name__ == "__main__":
     main()
