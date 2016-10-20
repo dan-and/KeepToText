152fb5c2748c91b61e56319a80ae2f1a4566b9db
Tzadik Vanderhoof <Tzadik.Vanderhoof@gmail.com>
2016-10-19 19:58:52 -0700

    tabs

--- html-to-text.py
+++ html-to-text.py
@@ -1,12 +1,12 @@
 import sys
 
 def main():
-	try:
-		cmd, dir, tag, attrib, attribVal = sys.argv
-	except ValueError as e:
-		print "Usage:", sys.argv[0], "dir tag attrib attribVal"
-		return;
-	print(tag)
+    try:
+        cmd, dir, tag, attrib, attribVal = sys.argv
+    except ValueError as e:
+        print "Usage:", sys.argv[0], "dir tag attrib attribVal"
+        return
+    print(tag)
 
 if __name__ == "__main__":
-	main()
+    main()
